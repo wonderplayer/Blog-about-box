05d015eee7c2b053c391a6a6f37fafb5ff6de4bc
unknown <22dajelc@WS15K315D12.rvt.lv>
2015-11-12 12:30:13 +0200

    Random numbers

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,25 @@
+<?php
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number/{count}")
+     */
+    public function numberAction($count)
+    {
+        $numbers = array();
+		for ($i=0; $i < $count; $i++){
+			$numbers[] = rand(0,100);
+		}
+		 $numbersList = implode(', ', $numbers);
+        return new Response(
+            '<html><body>Lucky numbers: '.$numbersList.'</body></html>'
+        );
+    }
+}
